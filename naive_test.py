85911232cd547966ac25fa97c96b8d4caf979742
esfrankel <esfrankel17@gmail.com>
2021-01-04 08:33:08 -0700

    changes to python tes scripts and added a naive tester

--- naive_test.py
+++ naive_test.py
@@ -9,27 +9,20 @@ from BanditPAM import KMedoids
 times = []
 
 def runtime_and_profile(dataset_size, fixed_perm = True, k=5):
-    print("running mnist-{}k-parallelized-cache-k-{}".format(dataset_size, k))
+    print("running mnist-{}k-naive-cache-k-{}".format(dataset_size, k))
     X = pd.read_csv('data/MNIST-{}k.csv'.format(dataset_size), sep=' ', header=None).to_numpy()
 
     # Fit the data with BanditPAM:
     start = timeit.default_timer()
-    if not fixed_perm:
-        test = False
-        name = "mnist-{}k-parallelized-cache-k-{}-{}".format(dataset_size, k, "no-fixed-perm")
-        print("no fixed perm")
-    else:
-        test = True
-        name = "mnist-{}k-parallelized-cache-k-{}-{}".format(dataset_size, k, "fixed-perm")
-        print("fixed perm")
-    kmed = KMedoids(n_medoids = k, algorithm = "BanditPAM", verbosity=1, logFilename=name,cache = True, use_fixed_perm = test)
+    name = "mnist-{}k-naive-cache-k-{}".format(dataset_size, k)
+    kmed = KMedoids(n_medoids = k, algorithm = "naive", verbosity=1, logFilename=name,cache = True)
     kmed.fit(X, 'L2', k, name)
     elapsed = start - timeit.default_timer()
     times.append(-elapsed)
     print(elapsed)
     print(kmed.final_medoids)
-for val in [20]:
-    runtime_and_profile(val, True)
+for val in [5]:
+    runtime_and_profile(val, False)
 
 # sizes = [1, 10, 20, 70]
 # pool = mp.Pool(processes=len(sizes))
