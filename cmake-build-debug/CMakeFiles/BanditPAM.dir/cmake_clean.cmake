d1dd7c173cee00850aed974afca2c05f17126d26
esfrankel <esfrankel17@gmail.com>
2021-01-04 08:28:22 -0700

    fixing C++ building locally with CMake

---
+++ cmake-build-debug/CMakeFiles/BanditPAM.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BanditPAM"
+  "BanditPAM.pdb"
+  "CMakeFiles/BanditPAM.dir/src/kmedoids_ucb.cpp.o"
+  "CMakeFiles/BanditPAM.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BanditPAM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
